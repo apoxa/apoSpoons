8d485d290a7b3dc8332934466d90c8d87563ed4f
Benjamin Stier <b.stier@levigo.de>
2022-04-21 10:53:50 +0200

    Mute input globally and not per app, because it might still be listening.
    See https://www.cultofmac.com/766481/zoom-listening-mac/

--- Source/MacroPad.spoon/init.lua
+++ Source/MacroPad.spoon/init.lua
@@ -6,9 +6,21 @@
 --- Things like mute and raiseHand support common meeting apps I use. If you need another one,
 --- feel free to add the keyStrokes to the lists.
 ---
+local application = require("hs.application")
+local watchable = require("hs.watchable")
+local logger = require("hs.logger")
+local audiodevice = require("hs.audiodevice")
 
 local obj = {}
 obj.__index = obj
+
+obj.watchables = watchable.new("macropad")
+obj.watchables.micState = "off"
+
+local function timestamp(date)
+    date = date or require"hs.timer".secondsSinceEpoch()
+    return os.date("%F %T" .. ((tostring(date):match("(%.%d+)$")) or ""), math.floor(date))
+end
 
 -- Metadata
 obj.name = "MacroPad"
@@ -17,7 +29,7 @@ obj.author = "Benjamin Stier <ben@unpatched.de>"
 obj.homepage = "https://github.com/apoxa/hammerspoon-macropad"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
-obj.logger = hs.logger.new('MacroPad')
+obj.logger = logger.new('MacroPad')
 
 --- MacroPad.serialPort
 --- Variable
@@ -40,7 +52,7 @@ obj.muteLED = {
     number = 0,
     off = "0 0 0",
     muted = "255 0 0",
-    unmuted = "0 255 0",
+    unmuted = "0 255 0"
 }
 
 --- MacroPad:bindHotkeys(mapping)
@@ -66,15 +78,13 @@ end
 ---  * None
 function obj:toggleMute()
     self.logger.v("toggleMute triggered")
-    triggeredApp = self:tryAppButtons('muteButtons', hs.fnutils.filter(self.meetingApps, function(meetingApp)
-        return meetingApp.muteButtons ~= nil
-    end))
-    if triggeredApp == nil then return end
-    if (self.meetingApps[triggeredApp].checkAudio ~= nil) then
-        state = self.meetingApps[triggeredApp].checkAudio() or 'off'
-        self.logger.df("Current Mic State in %s is %s", triggeredApp, state)
-        self.logger.df("Sending LED to %s with color %s", self.muteLED.number, self.muteLED[state])
-        self:_sendSerial(string.format("LED %s %s\r\n", self.muteLED.number, self.muteLED[state]))
+    local mic = audiodevice.defaultInputDevice()
+    if not mic:inUse() then
+        obj.watchables.micState = 'off'
+    elseif mic:inputMuted() then
+        mic:setInputMuted(false)
+    else
+        mic:setInputMuted(true)
     end
 end
 
@@ -94,7 +104,7 @@ end
 function obj:tryAppButtons(buttonType, apps)
     apps = apps or {}
     for application in pairs(apps) do
-        local app = hs.application.get(application)
+        local app = application.get(application)
         if app ~= nil then
             buttons = self.meetingApps[application]
             self.logger.df("found app for keyStrokes: %s\n", application)
@@ -113,65 +123,51 @@ function obj:_sendSerial(data)
     end
 end
 
-function obj:_checkZoomAudio()
-    local check = hs.application.get("us.zoom.xos")
-    if (check ~= nil) then
-        if check:findMenuItem({"Meeting", "Unmute Audio"}) then
-            return 'muted'
-        elseif check:findMenuItem({"Meeting", "Mute Audio"}) then
-            return "unmuted"
-        else
-            return "off"
-        end
+obj._micWatcher = watchable.watch('macropad', 'micState', function(_, _, _, _, new)
+    obj.logger.df("Current Mic state is %s", new)
+    if obj.muteLED[new] ~= nil then
+        obj:_sendSerial(string.format("LED %s %s\r\n", obj.muteLED.number, obj.muteLED[new]))
     end
-end
-
-function obj:_checkTeamsAudio()
-    local check = hs.application.get("com.microsoft.teams")
-    local axApp = hs.axuielement.applicationElement(check)
-    -- Fix accessability API in electron App
-    axApp:setAttributeValue('AXManualAccessibility', true)
-    -- AXDescription = "Mute (⌘+Shift+M)"
-    -- AXDescription = "Unmute (⌘+Shift+M)",
-    MicButtonSearch = hs.axuielement.searchCriteriaFunction({
-        { attribute = "AXRole", value = "AXButton"}, { attribute = "AXDOMIdentifier", value = "microphone-button" }
-    })
-    -- find window with Mute Button
-    for i, window in ipairs(axApp.AXWindows) do
-        axMicButton = window:elementSearch(nil, MicButtonSearch, {depth = 9, noCallback = true})[1]
-        if axMicButton ~= nil then
-            if axMicButton.AXDescription:match("Unmute") then
-                return 'muted'
-            elseif axMicButton.AXDescription:match("Mute") then
-                return 'unmuted'
-            else
-                return 'off'
-            end
-            return
-        end
-    end
-end
+end)
 
 function obj:init()
     self.meetingApps = {
         ['bbb'] = {
-            muteButtons = {{'ctrl', 'alt'}, 'm'},
-            raiseHandButtons = {{'ctrl', 'alt'}, 'r'},
+            raiseHandButtons = {{'ctrl', 'alt'}, 'r'}
         },
         ['com.microsoft.teams'] = {
-            muteButtons = {{'cmd', 'shift'}, 'm'},
-            raiseHandButtons = {{'cmd', 'shift'}, 'k'},
-            checkAudio = hs.fnutils.partial(self._checkTeamsAudio, self),
+            raiseHandButtons = {{'cmd', 'shift'}, 'k'}
         },
         ['us.zoom.xos'] = {
-            muteButtons = {{'cmd', 'shift'}, 'm'},
-            raiseHandButtons = {'alt', 'y'},
-            checkAudio = hs.fnutils.partial(self._checkZoomAudio, self),
-        },
-        ['com.hnc.Discord'] = {
-            muteButtons = {{"cmd", "shift"}, "a"},
-        },
+            raiseHandButtons = {'alt', 'y'}
+        }
     }
+    return self
+end
+
+function obj:start()
+    mic = audiodevice.defaultInputDevice():watcherCallback(function(UID, event, scope, element)
+        obj.logger.df("UID <%s>, event <%s>, scope <%s>, element <%s>", UID, event, scope, element)
+        if (event == 'gone' and scope == 'glob') then
+            if audiodevice.findDeviceByUID(UID):inUse() then
+                obj.watchables.micState = audiodevice.findDeviceByUID(UID):inputMuted() and 'muted' or 'unmuted'
+            else
+                obj.watchables.micState = 'off'
+            end
+        end
+        if (event == 'mute' and scope == 'inpt') then
+            obj.watchables.micState = audiodevice.findDeviceByUID(UID):inputMuted() and 'muted' or 'unmuted'
+        end
+    end)
+    mic:watcherStart()
+    return self
+end
+
+function obj:stop()
+    self._micWatcher:release()
+    audiodevice.watcher.stop()
+    mic = audiodevice.defaultInputDevice():watcherStop()
+    return self
 end
 
 return obj
